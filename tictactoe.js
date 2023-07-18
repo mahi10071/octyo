9852191637ba388b4a02a736e739f273ad855ab8
mahi10071 <108299081+mahi10071@users.noreply.github.com>
2023-07-19 00:58:03 +0530

    tictactoe.js

--- tictactoe.js
+++ tictactoe.js
@@ -20,7 +20,7 @@ function checkWinner() {
     document.write(`Draw!!`);
     return;
   }
-  }
+}
 
 function handleClick(el) {
   const id = Number(el.id);
@@ -28,27 +28,5 @@ function handleClick(el) {
   arr[id] = currentPlayer;
   el.innerText = currentPlayer;
   checkWinner();
-  currentPlet currentPlayer = "X";
-let arr = Array(9).fill(null);
-
-function checkWinner() {
-  if (
-    (arr[0] !== null && arr[0] == arr[1] && arr[1] == arr[2]) ||
-    (arr[3] !== null && arr[3] == arr[4] && arr[4] == arr[5]) ||
-    (arr[6] !== null && arr[6] == arr[7] && arr[7] == arr[8]) ||
-    (arr[0] !== null && arr[0] == arr[3] && arr[3] == arr[6]) ||
-    (arr[1] !== null && arr[1] == arr[4] && arr[4] == arr[7]) ||
-    (arr[2] !== null && arr[2] == arr[5] && arr[5] == arr[8]) ||
-    (arr[0] !== null && arr[0] == arr[4] && arr[4] == arr[8]) ||
-    (arr[2] !== null && arr[2] == arr[4] && arr[4] == arr[6])
-  ) {
-    document.write(`Winner is ${currentPlayer}`);
-    return;
-  }
-
-  if (!arr.some((e) => e === null)) {
-    document.write(`Draw!!`);
-    return;
-  }
-}layer = currentPlayer === "X" ? "O" : "X";
+  currentPlayer = currentPlayer === "X" ? "O" : "X";
 }
